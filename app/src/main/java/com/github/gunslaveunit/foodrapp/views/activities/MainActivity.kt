9411e97a6233598d98f55c15a66be56c1d45ee30
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-10-26 13:26:37 +0300

    Main activity moved to activities package

--- app/src/main/java/com/github/gunslaveunit/foodrapp/views/activities/MainActivity.kt
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/views/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.gunslaveunit.foodrapp
+package com.github.gunslaveunit.foodrapp.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.gunslaveunit.foodrapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
