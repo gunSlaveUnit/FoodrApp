d1849fae283fc563d8291378a9e7592ae689d517
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-06 22:15:19 +0300

    Parcelize annotation and class

--- app/src/main/java/com/github/gunslaveunit/foodrapp/models/Table.kt
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/models/Table.kt
@@ -1,7 +1,11 @@
 package com.github.gunslaveunit.foodrapp.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Table(
     val number: Int,
     val description: String,
     val capacity: Int
-)
+): Parcelable
