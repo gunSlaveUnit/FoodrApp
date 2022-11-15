4e143ab855d453470bfd66a7f22eda4859a1a77b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-15 11:59:49 +0300

    Added mapping of table number to NewBookingFragment

--- app/src/main/java/com/github/gunslaveunit/foodrapp/views/fragments/NewBookingFragment.kt
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/views/fragments/NewBookingFragment.kt
@@ -5,13 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.navArgs
 import com.github.gunslaveunit.foodrapp.R
 
 class NewBookingFragment : Fragment() {
+    private val args by navArgs<NewBookingFragmentArgs>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_new_booking, container, false)
+        val view = inflater.inflate(R.layout.fragment_new_booking, container, false)
+
+        view.findViewById<TextView>(R.id.new_booking_table_number).text = args.table.number.toString()
+
+        return view
     }
 }
