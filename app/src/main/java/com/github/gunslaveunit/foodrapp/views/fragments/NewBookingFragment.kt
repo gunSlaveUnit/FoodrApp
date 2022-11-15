ea22dc8c1df01ca3665d9cddb879c173b290851c
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-15 13:43:16 +0300

    Added click handler for cancel button

--- app/src/main/java/com/github/gunslaveunit/foodrapp/views/fragments/NewBookingFragment.kt
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/views/fragments/NewBookingFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.github.gunslaveunit.foodrapp.R
 
@@ -17,6 +19,10 @@ class NewBookingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_new_booking, container, false)
+
+        view.findViewById<Button>(R.id.cancel_new_booking_button).setOnClickListener{
+            findNavController().navigate(R.id.action_newBookingFragment_to_tablesListFragment)
+        }
 
         view.findViewById<TextView>(R.id.new_booking_table_number).text = args.table.number.toString()
 
