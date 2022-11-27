f3099aae4c736f0df8e3489e4b2fc36c9d434a95
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-27 19:07:07 +0300

    Added Entity annotation to Table class.

--- app/src/main/java/com/github/gunslaveunit/foodrapp/models/Table.kt
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/models/Table.kt
@@ -1,10 +1,15 @@
 package com.github.gunslaveunit.foodrapp.models
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
+@Entity(tableName="tables")
 @Parcelize
 data class Table(
+    @PrimaryKey(autoGenerate=true)
+    val id: Int,
     val number: Int,
     val description: String,
     val capacity: Int
