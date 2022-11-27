673624eb762514e15056c0c3ef6b038eb54533b7
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-27 19:48:08 +0300

    Added bookingDao to the db

--- app/src/main/java/com/github/gunslaveunit/foodrapp/data/database/database.kt
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/data/database/database.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.github.gunslaveunit.foodrapp.data.daos.BookingDao
 import com.github.gunslaveunit.foodrapp.data.daos.TableDao
 import com.github.gunslaveunit.foodrapp.data.models.Table
 
 @Database(entities = [Table::class], version = 1, exportSchema = false)
 abstract class FoodrAppDB : RoomDatabase() {
     abstract fun tableDao(): TableDao
+    abstract fun bookingDao(): BookingDao
 
     companion object {
         @Volatile
