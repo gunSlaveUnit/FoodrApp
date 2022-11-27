00e31a85ae86ff99785c38580ae42ba114b1d000
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-27 19:10:00 +0300

    Added table dao

---
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/data/daos/TableDao.kt
@@ -0,0 +1,16 @@
+package com.github.gunslaveunit.foodrapp.data.daos
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.github.gunslaveunit.foodrapp.data.models.Table
+
+@Dao
+interface TableDao {
+    @Query("SELECT * FROM tables")
+    fun getAll(): LiveData<List<Table>>
+
+    @Insert()
+    suspend fun add(doctor: Table)
+}
