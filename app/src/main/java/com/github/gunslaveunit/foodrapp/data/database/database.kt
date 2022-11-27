319fab3c92a035c3dc4318cfb35065452cddb267
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-27 19:17:10 +0300

    Added database class

---
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/data/database/database.kt
@@ -0,0 +1,34 @@
+package com.github.gunslaveunit.foodrapp.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.github.gunslaveunit.foodrapp.data.daos.TableDao
+import com.github.gunslaveunit.foodrapp.data.models.Table
+
+@Database(entities = [Table::class], version = 1, exportSchema = false)
+abstract class FoodrAppDB : RoomDatabase() {
+    abstract fun tableDao(): TableDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: FoodrAppDB? = null
+
+        fun getDatabase(context: Context): FoodrAppDB {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room
+                    .databaseBuilder(
+                        context.applicationContext,
+                        FoodrAppDB::class.java,
+                        "foodrappdb"
+                    )
+                    .build()
+
+                INSTANCE = instance
+
+                instance
+            }
+        }
+    }
+}
