e19b5f4a4f82e54a8f6153a40dcb56b1f3ac95f8
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-28 20:56:16 +0300

    Added converters annotation to db class

--- app/src/main/java/com/github/gunslaveunit/foodrapp/data/database/database.kt
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/data/database/database.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.github.gunslaveunit.foodrapp.data.daos.BookingDao
 import com.github.gunslaveunit.foodrapp.data.daos.TableDao
 import com.github.gunslaveunit.foodrapp.data.models.Booking
 import com.github.gunslaveunit.foodrapp.data.models.Table
+import com.github.gunslaveunit.foodrapp.utils.converters.BitmapConverter
 
 @Database(entities = [Table::class, Booking::class], version = 1, exportSchema = false)
+@TypeConverters(value = [BitmapConverter::class])
 abstract class FoodrAppDB : RoomDatabase() {
     abstract fun tableDao(): TableDao
     abstract fun bookingDao(): BookingDao
