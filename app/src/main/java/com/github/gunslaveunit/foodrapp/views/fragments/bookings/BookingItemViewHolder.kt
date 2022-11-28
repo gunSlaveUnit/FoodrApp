fbdfbda40706e04383c302d3397697fadc9b7de6
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-11-28 20:23:19 +0300

    Created booking item view holder

---
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/views/fragments/bookings/BookingItemViewHolder.kt
@@ -0,0 +1,7 @@
+package com.github.gunslaveunit.foodrapp.views.fragments.bookings
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class BookingItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+}
