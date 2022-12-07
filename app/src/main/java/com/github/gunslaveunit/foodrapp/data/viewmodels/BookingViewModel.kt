75bb795ce6f6a0843d85a4aa834be8b0c471d2a9
iamcrysun <adamova.01@mail.ru>
2022-12-08 00:37:20 +0300

    Added one more method to add a booking but it uses a booking service

--- app/src/main/java/com/github/gunslaveunit/foodrapp/data/viewmodels/BookingViewModel.kt
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/data/viewmodels/BookingViewModel.kt
@@ -7,21 +7,30 @@ import androidx.lifecycle.viewModelScope
 import com.github.gunslaveunit.foodrapp.data.database.FoodrAppDB
 import com.github.gunslaveunit.foodrapp.data.models.Booking
 import com.github.gunslaveunit.foodrapp.data.repositories.BookingRepository
+import com.github.gunslaveunit.foodrapp.utils.services.BookingService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class BookingViewModel(application: Application): AndroidViewModel(application) {
     private val bookingRepository: BookingRepository
     private val all: LiveData<List<Booking>>
+    private val bookingService: BookingService
 
     init {
         val bookingDao = FoodrAppDB.getDatabase(application).bookingDao()
         bookingRepository = BookingRepository(bookingDao)
         all = bookingRepository.all
+        bookingService = BookingService(bookingRepository)
     }
 
     fun all(): LiveData<List<Booking>> {
         return all
+    }
+
+    fun make(booking: Booking) {
+        viewModelScope.launch(Dispatchers.IO) {
+            bookingService.make(booking)
+        }
     }
 
     fun add(booking: Booking) {
