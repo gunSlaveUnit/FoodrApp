5d86a58e82b913f9f88988a55f3f36a9bf213e7c
iamcrysun <adamova.01@mail.ru>
2022-12-08 00:21:12 +0300

    If there are not tables in the database it includes by initializer

--- app/src/main/java/com/github/gunslaveunit/foodrapp/views/fragments/tables/TablesListFragment.kt
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/views/fragments/tables/TablesListFragment.kt
@@ -1,17 +1,21 @@
 package com.github.gunslaveunit.foodrapp.views.fragments.tables
 
 import ListAdapter
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.StrictMode
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.gunslaveunit.foodrapp.R
+import com.github.gunslaveunit.foodrapp.data.models.Table
 import com.github.gunslaveunit.foodrapp.data.viewmodels.TableViewModel
+import com.github.gunslaveunit.foodrapp.utils.database.Initializer
+import java.net.URL
 
 class TablesListFragment : Fragment() {
     private lateinit var tableViewModel: TableViewModel
@@ -20,9 +24,17 @@ class TablesListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
         val view = inflater.inflate(R.layout.fragment_tables_list, container, false)
 
         tableViewModel = ViewModelProvider(this)[TableViewModel::class.java]
+
+        tableViewModel.all().observe(viewLifecycleOwner) { tables ->
+            if (tables.isEmpty())
+                prepopulateData()
+        }
 
         val adapter = ListAdapter()
         val tablesListView = view.findViewById<RecyclerView>(R.id.tablesList)
@@ -34,5 +46,10 @@ class TablesListFragment : Fragment() {
         }
 
         return view
+    }
+
+    private fun prepopulateData() {
+        val initializer = Initializer()
+        initializer.prepopulateTables(tableViewModel)
     }
 }
