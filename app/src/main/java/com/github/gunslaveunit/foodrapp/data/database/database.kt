628dfcee601a4d6e42c1a5a7f02bfebeac221082
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-12-08 16:49:44 +0300

    Removed date converter

--- app/src/main/java/com/github/gunslaveunit/foodrapp/data/database/database.kt
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/data/database/database.kt
@@ -10,10 +10,9 @@ import com.github.gunslaveunit.foodrapp.data.daos.TableDao
 import com.github.gunslaveunit.foodrapp.data.models.Booking
 import com.github.gunslaveunit.foodrapp.data.models.Table
 import com.github.gunslaveunit.foodrapp.utils.converters.BitmapConverter
-import com.github.gunslaveunit.foodrapp.utils.converters.DateConverter
 
 @Database(entities = [Table::class, Booking::class], version = 1, exportSchema = false)
-@TypeConverters(value = [BitmapConverter::class, DateConverter::class])
+@TypeConverters(value = [BitmapConverter::class])
 abstract class FoodrAppDB : RoomDatabase() {
     abstract fun tableDao(): TableDao
     abstract fun bookingDao(): BookingDao
