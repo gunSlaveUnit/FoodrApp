8d3ebd09f597f9b944133ea6e4d6b9546e304bee
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-12-09 10:50:17 +0300

    Dish model is parceable now

--- app/src/main/java/com/github/gunslaveunit/foodrapp/data/models/Dish.kt
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/data/models/Dish.kt
@@ -1,10 +1,13 @@
 package com.github.gunslaveunit.foodrapp.data.models
 
 import android.graphics.Bitmap
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Entity(tableName="dishes")
+@Parcelize
 data class Dish (
     @PrimaryKey(autoGenerate=true)
     val id: Int,
@@ -15,4 +18,4 @@ data class Dish (
     val composition: String,
     val price: Int,
     val photo: Bitmap
-)
+): Parcelable
