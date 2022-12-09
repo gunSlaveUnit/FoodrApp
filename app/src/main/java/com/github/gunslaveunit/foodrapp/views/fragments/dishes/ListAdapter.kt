acf46e2eed5fa99cd640411e37823dc965225a97
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-12-09 10:50:48 +0300

    Added on click listener to food list item. It navigates to dish detailed fragment

--- app/src/main/java/com/github/gunslaveunit/foodrapp/views/fragments/dishes/ListAdapter.kt
+++ app/src/main/java/com/github/gunslaveunit/foodrapp/views/fragments/dishes/ListAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.github.gunslaveunit.foodrapp.R
 import com.github.gunslaveunit.foodrapp.data.models.Dish
@@ -26,6 +28,11 @@ class ListAdapter: RecyclerView.Adapter<DishItemViewHolder>() {
         holder.itemView.findViewById<TextView>(R.id.dishTitle).text = item.title
         holder.itemView.findViewById<TextView>(R.id.dishPrice).text = item.price.toString()
         holder.itemView.findViewById<ImageView>(R.id.photo2).setImageBitmap(item.photo)
+
+        holder.itemView.findViewById<ConstraintLayout>(R.id.dish_item).setOnClickListener {
+            val action = FoodListFragmentDirections.actionFoodListFragmentToDetailedDishFragment(item)
+            holder.itemView.findNavController().navigate(action)
+        }
     }
 
     override fun getItemCount(): Int {
